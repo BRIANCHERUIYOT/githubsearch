2ac1925f72e411355a1b67213cccf2eab91b875f
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-17 20:09:13 +0300

    adds bootstrap

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule, HttpHeaders,HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  // private httpClient: HttpClient
   constructor() { }
 }
