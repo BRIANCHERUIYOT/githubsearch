8c9c49d9db3cedf0aa9aebe7441f9ef478eda14a
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-17 21:59:37 +0300

    adds all the function for api

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpClientModule, HttpHeaders,HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,14 @@ import {HttpClient, HttpClientModule, HttpHeaders,HttpParams} from '@angular/com
 export class UserService {
 
   constructor(private httpClient: HttpClient) { }
+  getUser(): Observable<githubsearch[]>{
+    // let param = new HttpParams();
+    // param = param.append('username','BRIANCHERUIYOT');
+    return this.httpClient.get<githubsearch[]>('http://api.github.io.users',{
+      // params: param;/ 
+      // headers:this.headers,
+      withCredentials:true
+
+    })
+  }
 }
