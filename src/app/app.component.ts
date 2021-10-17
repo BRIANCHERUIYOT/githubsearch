fcd10d98cdb7f8df01d5d7cbddaf841cf043cd9f
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-17 18:54:31 +0300

    removes regidtration of services on app.componemts.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
-import { RepoService } from './repo/repo.service';
-import { UserService } from './user/user.service';
+import { Component } from '@angular/core'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[UserService,RepoService],
+ 
 })
 export class AppComponent {
   title = 'githubsearch';
