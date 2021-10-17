5da0fc4febf4e45dacf55508b2ff47c8d15a879b
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-17 18:24:23 +0300

    registers repo service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RepoService } from './repo/repo.service';
 import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[UserService]
+  providers:[UserService,RepoService],
 })
 export class AppComponent {
   title = 'githubsearch';
