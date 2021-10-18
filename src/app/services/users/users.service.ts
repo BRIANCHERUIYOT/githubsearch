a748ff91250ded5f97a105f8765de94a19ebc4ef
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-18 11:39:36 +0300

    creates new class

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Users } from 'src/app/users';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  users!: Users
+getusers(){
+  let promise = new Promise((resolve,reject) => {
+    this.http.get<users>('https://api.github.com/users')
 
-  constructor() { }
+
+  })
+}
+
+  constructor(private http: HttpClient) { }
 }
